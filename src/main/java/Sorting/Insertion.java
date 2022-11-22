29e5583bdb193e46289e923b5965f22534caa168
MayankShivhare999 <mayank.shivhare999@gmail.com>
2022-11-22 12:35:09 +0530

    Commit

--- src/main/java/Sorting/Insertion.java
+++ src/main/java/Sorting/Insertion.java
@@ -1,21 +1,31 @@
 package Sorting;
 
 public class Insertion {
-    public static void main(String[] args) {
-        int[] arr = {4,6,2,5,7,9,1,3};
 
+    /**
+     * insertion sort algorithm to sort the array value
+     * Time Complexity - O(n-square)
+     * Space Complexity - O(1)
+     *
+     * @param arr
+     */
+    public static void insertionSort(int[] arr) {
         for(int i=1; i<arr.length; i++) {
             int temp = arr[i];
-            int j=i-1;
+            int j = i-1;
             while(j>=0 && arr[j]>temp) {
                 arr[j+1] = arr[j];
                 j--;
             }
             arr[j+1] = temp;
         }
+    }
 
+    public static void main(String[] args) {
+        int[] arr = {4,6,2,5,7,9,1,3};
+        insertionSort(arr);
         for(int x:arr) {
-            System.out.print(x+" ");
+            System.out.println(x);
         }
     }
 }
--- src/main/java/Sorting/Selection.java
+++ src/main/java/Sorting/Selection.java
@@ -1,26 +1,39 @@
 package Sorting;
 
 public class Selection {
-    public static void main(String[] args) {
-        int[] arr = {4,6,2,5,7,9,1,3};
 
+    /**
+     * Selection Sort to sort the values of Array
+     * Time Complexity - O(n-sqaure)
+     * Space Complexity - O(1)
+     *
+     * @param arr
+     */
+    public static void selectionSort(int[] arr) {
+        if(arr == null && arr.length<1) {
+            return;
+        }
         for(int i=0; i<arr.length-1; i++) {
+            int j = i+1;
             int min = i;
-            for(int j=i+1; j<arr.length; j++) {
+            while(j<arr.length) {
                 if(arr[j]<arr[min]) {
                     min = j;
                 }
+                j++;
             }
-            swap(arr, i, min);
-        }
-        for(int x:arr) {
-            System.out.print(x+" ");
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
         }
     }
 
-    static void swap(int[] arr, int l, int r) {
-        int temp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = temp;
+    public static void main(String[] args) {
+        int[] arr = {4,6,2,5,7,9,1,3};
+
+        selectionSort(arr);
+        for(int x: arr) {
+            System.out.println(x);
+        }
     }
 }
